ca303bb840f4d5c0ac57733e0344739f0890f6c1
lavijiang <lavijiang@tencent.com>
2022-04-01 19:37:41 +0800

    games101 hw0

--- games-101/Assignment0/main.cpp
+++ games-101/Assignment0/main.cpp
@@ -2,7 +2,8 @@
 #include<eigen3/Eigen/Core>
 #include<eigen3/Eigen/Dense>
 #include<iostream>
-
+#define PI 3.1415926f
+#define DEG2RAD (PI/180.0f)
 int main(){
 
     // Basic Example of cpp
@@ -51,5 +52,16 @@ int main(){
     // TO DO: Define point P
     // TO DO: Define rotation matrix M
     // TO DO: M * P
+    Eigen::Vector3f P(2,1,1);
+    Eigen::Matrix3f M;
+
+    float rad = 45.0f * DEG2RAD;
+    M << cos(rad),-sin(rad),1,
+         sin(rad),cos(rad) ,2,
+         0,0,1;
+    
+    Eigen::Vector3f P2 = M * P;
+    std::cout<<P2<<std::endl;
+
     return 0;
 }
